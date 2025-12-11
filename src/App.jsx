6b2d4ecc77fa4f042d87299219f8aad5b8934dd1
Himanshu Gupta <guptahimanshu79087@gmail.com>
2025-12-11 23:23:59 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Collections from './pages/Collections'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+ import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   return (
     <div className = 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       <Navbar/>
+      <SearchBar/>
       <Routes>
         <Route path = '/' element={<Home/>}/>
         <Route path = '/collection' element={<Collections/>}/>
@@ -23,9 +28,10 @@ const App = () => {
         <Route path = '/product/:productId' element={<Product/>}/>
         <Route path = '/login' element={<Login/>}/>
         <Route path = '/cart' element={<Cart/>}/>
-        <Route path = '/placeorder' element={<PlaceOrder/>}/>
+        <Route path = '/place-order' element={<PlaceOrder/>}/>
         <Route path = '/orders' element={<Orders/>}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,5 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
